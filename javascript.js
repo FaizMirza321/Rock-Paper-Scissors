ad8ce943617214a0302f44f4c09e6ef4bf8befd7
Faiz Mirza <141449876+FaizMirza321@users.noreply.github.com>
2024-09-14 22:13:12 +0100

    Created functions for the round and choices

--- javascript.js
+++ javascript.js
@@ -1,1 +1,50 @@
-console.log("Hello World")
+let humanScore = 0;
+let computerScore = 0;
+
+function numtoChoice(num){
+    if (num==0){
+        return "rock";
+    }
+    else if(num==1){
+        return "paper";
+    }
+    else if (num==2){
+        return "scissors";
+    }
+        
+}
+function getComputerChoice(min, max){
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    let num = Math.floor(Math.random() * (maxFloored - minCeiled)+ minCeiled);
+    return numtoChoice(num);
+}
+
+function getHumanChoice(){
+    let choice = prompt("rock, paper or scissors?").toLowerCase;
+    while (choice !="rock" || "paper" || "scissors"){
+        console.log("invalid choice");
+        choice = prompt("rock, paper or scissors?").toLowerCase;
+    }
+    return choice;
+
+}
+function playRound(humanChoice, computerChoice){
+    console.log(humanChoice + "human")
+    console.log(computerChoice + "computer")
+    if(humanChoice == "rock" && computerChoice == "scissors"){
+        humanScore+=1;
+        console.log("You won this round! " + humanChoice + "beats " + computerChoice)
+    }
+    else if (humanChoice=="scissors" && computerChoice=="paper"){
+        humanScore+=1;
+        console.log("You won this round! " + humanChoice + "beats " + computerChoice)
+    }
+    else if(humanChoice==computerChoice){
+        console.log("Tie round, you both picked:" + humanChoice)
+    }
+    else{
+        computerScore+=1;
+        console.log("You lost this round! " + computerChoice + "beats " + humanChoice)
+    }
+}
